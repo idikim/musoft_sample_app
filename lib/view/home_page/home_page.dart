82244635a8a7ab966bb9c319f1b889ef732633f7
hyeji_kim <plithus.khj@gmail.com>
2025-07-24 14:12:19 +0900

    feat: 홈 화면 수정 (fl_chart 패키지 추가)

--- lib/view/home_page/home_page.dart
+++ lib/view/home_page/home_page.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _topTabIndex = 2;
+  int _topTabIndex = 0;
   int _mealTabIndex = 0;
 
   @override
